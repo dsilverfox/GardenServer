1d21b5781c19b9cbf76e13a7e4ad5d9706b8d37b
Michelle Owsley <michowsley@gmail.com>
2021-12-14 21:43:39 -0500

    app.headers commit

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ const Express = require("express");
 const app = Express();
 const dbConnection = require("./db");
 app.use(Express.json());
+app.use(require('./middleware/headers'));
+
 
 app.listen(3000, () => {
     console.log(`[Server]: App is listening on Port 3000`)
