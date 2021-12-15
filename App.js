62adf03b485e26a04085926af346cdd4c45b3272
Danielle Joy <daniellejoy@danielles-mbp.lan>
2021-12-15 18:08:53 -0500

    because github is yelling at me again

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ const Express = require("express");
 const app = Express();
 const dbConnection = require("./db");
 const controllers = require('./controllers')
-const headers =require('./headers')
+
+
+
 app.use(Express.json());
 app.use(require('./middleware/headers'));
 
