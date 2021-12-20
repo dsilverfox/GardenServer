ce697ce23f61374049b5716c9dca18ff58f98fae
Danielle Joy <daniellejoy@danielles-mbp.lan>
2021-12-20 16:38:28 -0500

    changed DB info

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 });
