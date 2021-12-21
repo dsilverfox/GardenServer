fde1e9c47cc66bd6bec411f1de3b23644fcf8932
Danielle Joy <daniellejoy@danielles-mbp.lan>
2021-12-21 17:37:04 -0500

    try to fix ssl

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:'postgres',
-    ssl: process.env.ENVIRONMENT === 'production'
+    ssl:
+        process.env.ENVIRONMENT === 'production',
+        rejectUnauthorized: false
 });
 
 module.exports = sequelize;
